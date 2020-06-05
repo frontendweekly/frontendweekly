4c63c93b9684d550f30a72cd7b1fdd78afa6f6f1
Kota <kaityo02@gmail.com>
2020-06-05 13:51:29 +0900

    fix

--- _task/node-trello-contents.js
+++ _task/node-trello-contents.js
@@ -193,12 +193,12 @@ ${generateInBriefHeading()}
 ${generateInbrief(tmplData)}`;
   };
 
-  const title = options.title ? `Vol.${options.title}` : `Vol.${getNextVol()}`;
+  const vol = options.title || getNextVol();
   return matter.stringify(file(), {
-    title,
+    title: `Vol.${vol}`,
     date: options.date || getNextWednesday(),
     desc: `3 OF TRANSLATED TITLE、ほか計${tmplData.length}リンク`,
-    permalink: `/posts/${title}/`,
+    permalink: `/posts/${vol}/`,
   });
 };
 
