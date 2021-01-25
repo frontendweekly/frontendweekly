0d4df6f91e40a64a9e3d130646cd25dae901ebbc
Yuya Saito <hello@studiomohawk.com>
2021-01-25 21:40:37 +0900

    Lint

--- 11ty/_includes/partials/global/service-worker.js
+++ 11ty/_includes/partials/global/service-worker.js
@@ -17,14 +17,15 @@ const IGNORED_HOSTS = ['localhost'];
 /**
  * Takes an array of strings and puts them in a named cache store
  *
- * @param {String} cacheName
+ * @param {string} cacheName
  * @param {Array} items=[]
+ * @param items
  */
 const addItemsToCache = function (cacheName, items = []) {
   caches.open(cacheName).then((cache) => cache.addAll(items));
 };
 
-self.addEventListener('install', (evt) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
 
   addItemsToCache(CACHE_KEYS.PRE_CACHE, PRE_CACHE_URLS);
@@ -80,7 +81,7 @@ self.addEventListener('fetch', (evt) => {
               return response;
             });
           })
-          .catch((ex) => {});
+          .catch(() => {});
       });
     })
   );
