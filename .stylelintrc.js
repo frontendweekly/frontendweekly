1f70986324f0bd6cb48fb151a0549744d287ea4d
Yuya Saito <hello@studiomohawk.com>
2021-09-19 11:31:21 +0900

    chore: update deps

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: '@frontendweekly/stylelint-config',
+  rules: {
+    'csstools/value-no-unknown-custom-properties': [
+      true,
+      {
+        severity: 'warning',
+      },
+    ],
+  },
 };
